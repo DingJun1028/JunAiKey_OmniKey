50b42594c4c54e945542c39eff766427c231ec97
Jun Hong <dingjunhong1028@gmail.com>
2025-06-28 00:40:33 +0800

    chore: 暫存所有本地未追蹤檔案以解決 rebase/pull 衝突

--- JunAiKey [手機板]/junaikey (手機編碼)/supabase/functions/_shared/cors.ts
+++
@@ -1,4 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Or restrict to your frontend origin
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
